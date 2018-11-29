fba8475371da711e0e0784de42367069d464c674
yiming chen <bombcoder@outlook.com>
2018-11-29 15:31:26 +0800

    update getdata.sh

--- getdata.sh
+++ getdata.sh
@@ -5,7 +5,7 @@ export OUTPUT_PATH=/root/output
 export HADOOP_PREFIX=/usr/local/hadoop
 
 pushd ${DATA_PATH}
-curl -O ${DATA_PRODUCE}
+curl -O ${DATA_PRODUCE}/${FILE}
 ${HADOOP_PREFIX}/bin/hadoop fs -mkdir -p /kaola/order/
 ${HADOOP_PREFIX}/bin/hadoop fs -moveFromLocal . /kaola/order/input
 popd
