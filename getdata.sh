109267cbba0271f3d3d7389a4e92d86ce6507951
yiming chen <bombcoder@outlook.com>
2018-11-29 15:27:18 +0800

    add dockerfile

---
+++ getdata.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+export DATA_PATH=/root/input
+export OUTPUT_PATH=/root/output
+export HADOOP_PREFIX=/usr/local/hadoop
+
+pushd ${DATA_PATH}
+curl -O ${DATA_PRODUCE}
+${HADOOP_PREFIX}/bin/hadoop fs -mkdir -p /kaola/order/
+${HADOOP_PREFIX}/bin/hadoop fs -moveFromLocal . /kaola/order/input
+popd
