842875e24a491a6ca38cb2e917c261a4c30f021d
yiming chen <bombcoder@outlook.com>
2018-11-30 09:31:44 +0800

    update dockerfile and getdatea.sh

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM sequenceiq/hadoop-docker
 RUN yum install -y yum-plugin-ovl ; yum clean all
 RUN yum install -y git ;  yum clean all
 RUN yum update -y nss nss-util nspr 
-RUN mkdir /root/output && /root/code && /root/input
+RUN mkdir /root/output &&mkdir /root/code &&mkdir /root/input
 COPY src /root/code
 COPY run.sh /root/code
 COPY README.md /root/code
--- getdata.sh
+++ getdata.sh
@@ -4,8 +4,6 @@ export DATA_PATH=/root/input
 export OUTPUT_PATH=/root/output
 export HADOOP_PREFIX=/usr/local/hadoop
 
-pushd ${DATA_PATH}
-curl -O ${DATA_PRODUCE}/${FILE}
+curl -o ${DATA_PATH}/data  ${DATA_PRODUCE}/output.txt
 ${HADOOP_PREFIX}/bin/hadoop fs -mkdir -p /kaola/order/
-${HADOOP_PREFIX}/bin/hadoop fs -moveFromLocal . /kaola/order/input
-popd
+${HADOOP_PREFIX}/bin/hadoop fs -moveFromLocal ${DATA_PATH}/data /kaola/order/input
